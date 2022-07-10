8e89e037d5759c61aa8e92eb7dad7abac39c8152
BS-Kim-Dev <bumsi0426@gmail.com>
2022-07-10 13:55:17 +0900

    add remark

--- AVL_tree.cpp
+++ AVL_tree.cpp
@@ -1,14 +1,15 @@
 ﻿#include <iostream>
 
+
 typedef struct Node {
-    int data;
-    int height;
-    struct Node *leftChild;
-    struct Node *rightChild;
+    int data;                   // 우선 순위, 트리에 자료 넣을 때 정렬 기준
+    int height;                 // 트리의 높이 또는 깊이
+    struct Node *leftChild;     // 왼쪽 자식 노드
+    struct Node *rightChild;    // 오른쪽 자식 노드
 } Node;
 
 int getMax(int a, int b){
-    if (a > b){return a;}
+    if (a > b){return a;}       
     else {return b;}
 }
 
@@ -27,14 +28,14 @@ int getDifference(Node *node){
 }
 
 Node* insertNode (Node *root, int data){
-    if (root == NULL) {
+    if (root == NULL) {                             // 최초 데이터 입력 시 왼쪽/오른쪽 자식 노드는 NULL로 정의
         root = (Node*)malloc(sizeof(Node));
         root->leftChild = root->rightChild = NULL;
         root->data = data;
         return root;
     }
-    else {
-        if (root->data > data) {
+    else {                                          // 기존 데이터가 있을 시, 새로 입력받은 data 와 노드의 data 를 비교하여
+        if (root->data > data) {                    // 왼쪽 or 오른쪽 자식 노드에 재귀적으로 넣어준다.
             root->leftChild = insertNode(root->leftChild, data);
         }
         else {
@@ -45,8 +46,8 @@ Node* insertNode (Node *root, int data){
 }
 
 Node* searchNode(Node *root, int data){
-    if (root == NULL) {return NULL;}
-    if (root->data == data) {return root;}
+    if (root == NULL) {return NULL;}                // 입력받은 데이터와, 노드의 데이터를 비교하여
+    if (root->data == data) {return root;}          // 같은 게 나올때까지 또는 자식 노드가 NULL 값이 나올때까지 재귀적으로 찾는다.
     else if (root->data > data) {return searchNode(root->leftChild, data);}
     else {return searchNode(root->rightChild, data);}
 }
@@ -92,6 +93,10 @@ Node* deleteNode(Node *root, int data){
     }
     return root;
 }
+
+// 여기까지는 일반적인 이진 탐색 트리,
+// 아래부터는 이진 탐색 트리가 편향되는 것을 막기 위해
+// 이진 탐색 트리를 AVL 트리로 유지시켜주는 ratate 함수 (LL, RR, LR, RL 회전)
 
 Node* rotateLL(Node* node){
     Node *leftChild = node->leftChild;
